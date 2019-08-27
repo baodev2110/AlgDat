35efc328a2228466fe614b7eb45062029a7df880
Bao <bao91@protonmail.com>
2019-08-27 10:44:18 +0200

    Initial commit

---
+++ src/Finn.java
@@ -0,0 +1,55 @@
+import java.util.NoSuchElementException;
+
+public class Finn
+{
+    public static int min(int[] a)
+    {
+        if(a.length < 1)
+            throw new NoSuchElementException("Tabellen er tom!");
+
+        int m = 0;
+        for(int i = 1; i <a.length; i++)
+            if(a[i] < a[m]) m = i;
+
+
+        return m;
+    }
+
+    public static int[] minmaks(int[] a)
+    {
+        int min = 0;
+        int max = 0;
+
+        for(int i = 1; i < a.length; i++)
+        {
+            if(a[i] < a[min])
+                min = a[i];
+
+            if(a[i] > a[max])
+                max = a[i];
+        }
+
+        return new int[]{min,max};
+    }
+
+    public static int maks(int[] a)   // versjon 2 av maks-metoden
+    {
+        int m = 0;               // indeks til største verdi
+        int maksverdi = a[0];    // største verdi
+
+        for (int i = 1; i < a.length; i++)
+        {
+            if (a[i] >= maksverdi)
+            {
+                maksverdi = a[i];     // største verdi oppdateres
+                m = i;                // indeks til største verdi oppdateres
+            }
+        }
+        return m;   // returnerer indeks/posisjonen til største verdi
+
+        // [10, 5, 7, 2, 9, 1, 3, 8, 4, 6]
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    } // maks
+
+}
---
+++ src/FinnTest.java
@@ -0,0 +1,19 @@
+
+class FinnTest
+{
+
+    @org.junit.jupiter.api.Test
+    void min()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minmaks()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void maks()
+    {
+    }
+}
